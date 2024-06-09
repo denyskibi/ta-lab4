bb35617cc59a9e3cbe4648a72ee407e36722aead
Denys K <github@kibitkin.com>
2024-06-09 22:28:15 +0300

    Added and particularly developed password generated working on radix and counting sorting

--- core/password_generator.py
+++ core/password_generator.py
@@ -1,12 +1,11 @@
 # Standard Libraries
 from typing import List
+from collections import Counter
 
 
 # Custom Modules
 from core.counting_sort import CountingSort
 from core.radix_sort import RadixSort
-
-from utils import file_utils
 
 
 class PasswordGenerator:
@@ -14,11 +13,15 @@ class PasswordGenerator:
         self._counting_sort = CountingSort()
         self._radix_sort = RadixSort(self._counting_sort)
 
-    def generate_password(self, loaded_anagrams: List[str]) -> str:
+    def generate_password(self, anagrams: List[str]) -> str:
         # Step #1: Perform Radix sorting for loaded anagrams
-        sorted_anagrams = self._radix_sort.sort(array=loaded_anagrams)
+        sorted_anagrams = self._radix_sort.sort(array=anagrams)
 
-        pass
+        # Step #2: Finding the most common letter among all anagrams
+        most_common_letter = self._found_most_common_latter(anagrams)
 
-    def _found_most_common_latter(self):
-        pass
+    @staticmethod
+    def _found_most_common_latter(anagrams: List[str]) -> str:
+        anagrams_str = ''.join(anagrams)  # merging anagrams to one string
+        most_common_latter = Counter(anagrams_str).most_common(1)[0][0]
+        return most_common_latter
